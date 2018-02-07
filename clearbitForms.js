4bb34414f80408aa6efe143b7589e01f59e2f1ce
Hussam's Work <halmukhtar@financialforce.com>
2018-02-06 16:16:05 -0800

    2

--- clearbitForms.js
+++ clearbitForms.js
@@ -4,8 +4,6 @@ setTimeout(function(){
 	companyRow.append('<p class="additions">WHat</p>');
 	var newElem = $(".mktoForm .mktoFieldWrap").find("p.additions");
 	console.log(newElem);
+	newElem.click(function(){alert("OK");});
 }, 2000);
 
-$(".mktoForm .mktoFieldWrap").on("click", "p.additions", function(){
-	alert("OK");
-});
